d6deb09d9c7f9a0c8c07a46d7bfaa39979b12d81
riddhimanrana <riddhiman.rana@gmail.com>
2025-05-25 15:03:06 -0700

    refactor: update page props to use Promise for params in multiple components

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,14 +1,43 @@
+const fg = require('fast-glob')
+const path = require('path')
+
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://lets-assist.com/",
+  siteUrl: 'https://lets-assist.com/',
   generateRobotsTxt: true,
+  // make sure outDir is your public folder (default)
+  outDir: './public',
+
+  // ignore API / auth folders
+  exclude: ['/api/*', '/auth/*', '/error/*'],
+
+  // manually pick up all app routes
+  additionalPaths: async (config) => {
+    // find all your page.tsx (or .js/.jsx/.tsx) under /app
+    const pages = await fg('app/**/page.{js,jsx,ts,tsx}', {
+      cwd: process.cwd(),
+      dot: false,
+    })
+
+    return Promise.all(
+      pages.map((file) => {
+        // turn "app/foo/bar/page.tsx" → "/foo/bar"
+        let urlPath = file
+          .replace(/^app/, '')
+          .replace(/\/page\..+$/, '')
+        if (urlPath === '') urlPath = '/'
+        return config.transform(config, urlPath)
+      })
+    )
+  },
+
   robotsTxtOptions: {
     policies: [
       {
-        userAgent: "*",
+        userAgent: '*',
         allow: '/',
-        disallow: ["/api/", "/auth/", "/error/"],
+        disallow: ['/api/', '/auth/', '/error/'],
       },
     ],
   },
-};
+}
